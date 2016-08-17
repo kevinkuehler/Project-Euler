733966cecd800ccaa311497f5153e211777c37c2
Kevin Kuehler <kevin.kuehler@gmail.com>
2016-08-17 02:56:38 -0700

    Update p039.cpp

--- p039.cpp
+++ p039.cpp
@@ -5,6 +5,7 @@ https://github.com/kevinkuehler/Project-Euler
 
 ***************************************************************************
 
+Prompt:
 If p is the perimeter of a right angle triangle with integral length sides,
 {a,b,c}, there are exactly three solutions for p = 120.
 
