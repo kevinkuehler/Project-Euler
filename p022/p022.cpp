a456d736ad788a696fb6c13f33545fb4629b0fa0
Kevin Kuehler <kevin.kuehler@gmail.com>
2016-08-18 01:38:34 -0700

    Update p022.cpp

--- p022/p022.cpp
+++ p022/p022.cpp
@@ -3,7 +3,7 @@ Project Euler Problem 22
 Kevin Kuehler
 https://github.com/kevinkuehler/Project-Euler
 
-*****************************************************************************
+*******************************************************************************************
 
 Using names.txt (right click and 'Save Link/Target As...'), a 46K text file containing over
 five-thousand first names, begin by sorting it into alphabetical order. 
@@ -12,7 +12,7 @@ alphabetical position in the list to obtain a name score.
 
 For example, when the list is sorted into alphabetical order, COLIN, which is worth 
 3 + 15 + 12 + 9 + 14 = 53, is the 938th name in the list.
-So, COLIN would obtain a score of 938 × 53 = 49714.
+So, COLIN would obtain a score of 938 Ã— 53 = 49714.
 
 What is the total of all the name scores in the file?
 */
