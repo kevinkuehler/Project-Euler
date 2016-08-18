bda11d2471e764079f6c459df554d0684b5637c0
Kevin Kuehler <kevin.kuehler@gmail.com>
2016-08-18 03:33:45 -0700

    Add files via upload

--- p039.cpp
+++ p039.cpp
@@ -8,46 +8,39 @@ https://github.com/kevinkuehler/Project-Euler
 Prompt:
 If p is the perimeter of a right angle triangle with integral length sides,
 {a,b,c}, there are exactly three solutions for p = 120.
-
 {20,48,52}, {24,45,51}, {30,40,50}
-
 For which value of p <= 1000, is the number of solutions maximised?
 */
 #include <iostream>
 #include <math.h>
 #include <cmath>
-#include <ctime>
 #define N 1000
 using namespace std;
 
 int perimeter_right_tri(int, int);
-int main()
-{
-    int PTABLE[N] = {0};
+int main() {
+	int PTABLE[N] = { 0 };
 
-    for(int a = 499; a > 0; a--)
-    {
-        int b = 2, p = perimeter_right_tri(a,1);
-        while(p <= N && b < a)
-        {
-            if(p != -1 )
-                PTABLE[p-1]++;
-            b++;
-            p = perimeter_right_tri(a,b);
-        }
-    }
+	for (int a = 499; a > 0; a--) {
+		int b = 2, p = perimeter_right_tri(a, 1);
+		while (p <= N && b < a) {
+			if (p != -1)
+				PTABLE[p - 1]++;
+			b++;
+			p = perimeter_right_tri(a, b);
+		}
+	}
 
-    int max_count = PTABLE[1], max_p = 2;
-    for(int i = 2; i < N; i++)
-    {
-        if(PTABLE[i] > max_count)
-        {
-            max_count = PTABLE[i];
-            max_p = i+1 ;
-        }
-    }
-    cout << max_p << endl;
-    return 0;
+	int max_count = PTABLE[1], max_p = 2;
+	for (int i = 2; i < N; i++) {
+		if (PTABLE[i] > max_count) {
+			max_count = PTABLE[i];
+			max_p = i + 1;
+		}
+	}
+	cout << max_p << endl;
+
+	return 0;
 }
 
 /*
@@ -56,11 +49,7 @@ Uses the formula p=a+b+c and the fact that
 a^2+b^2= c^2 => c=sqrt(a^2+b^2).
 Only returns integer values of p and -1 if it is a dec
 */
-int perimeter_right_tri(int a, int b)
-{
-    double c  = sqrt(a*a+b*b), check ;
-    return modf(c,&check) == 0 ? a+b+c : -1;
+int perimeter_right_tri(int a, int b) {
+	double c = sqrt(a*a + b*b);
+	return c == floor(c) ? a + b + c : -1;
 }
-
-
-
