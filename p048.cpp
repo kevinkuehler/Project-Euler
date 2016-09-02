42344abbb4f3956227c2631021e9a8a92f5e114c
__panda <kevin.kuehler@gmail.com>
2016-09-02 00:09:49 -0700

    Update p048.cpp

--- p048.cpp
+++ p048.cpp
@@ -1,3 +1,15 @@
+/*
+Project Euler Problem 48
+Kevin Kuehler
+https://github.com/kevinkuehler/Project-Euler
+
+**************************************************************************
+
+Prompt:
+The series, 1^1 + 2^2 + 3^3 + ... + 10^10 = 10405071317.
+
+Find the last ten digits of the series, 1^1 + 2^2 + 3^3 + ... + 1000^1000.
+*/
 #include <iostream>
 #define N 1000
 using namespace std;
