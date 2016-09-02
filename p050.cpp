7e9564f718b3a477d497d6cbf46e0f378ae831af
__panda <kevin.kuehler@gmail.com>
2016-09-02 00:10:05 -0700

    Update p050.cpp

--- p050.cpp
+++ p050.cpp
@@ -5,6 +5,7 @@ https://github.com/kevinkuehler/Project-Euler
 
 **********************************************************************************************************************
 
+Prompt:
 The prime 41, can be written as the sum of six consecutive primes:
 41 = 2 + 3 + 5 + 7 + 11 + 13
 
