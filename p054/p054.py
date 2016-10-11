47327e1e9053ffcb125f81f4e8f612167172496c
__panda <kevin.kuehler@gmail.com>
2016-10-11 02:23:43 -0700

    Add files via upload

---
+++ p054/p054.py
@@ -0,0 +1,34 @@
+from collections import defaultdict
+
+def poker(cards):
+	h1, r1 = hand_rank(cards[:5]) 
+	h2, r2 = hand_rank(cards[5:])
+	if r1 > r2: return 1
+	elif r1 == r2:
+		for c1,c2 in zip(h1, h2):
+			if c1[::-1] != c2[::-1]: 
+				if c1[::-1] > c2[::-1]: return 1
+				break
+	return 0
+
+def card_value(x):
+    return {'T': 10,'J': 11,'Q': 12,'K': 13,'A': 14}.get(x,ord(x)-48)
+
+def hand_rank(hand):
+	occur = defaultdict(int)
+	flush = True
+	flush_type = hand[0][1]
+	for card in hand:
+		if flush: flush = flush_type == card[1]
+		occur[card_value(card[0])] += 1
+	occur = sorted(occur.items(), key=lambda t: (t[1], t[0]), reverse=True)
+	if len(occur) == 5:
+		straight = (occur[0][0] - occur[-1][0] == 4)*36
+		if not straight: # ace wraps
+			straight = (occur[-1][0] == 2 and occur[1][0] == 5 and occur[0][0] == 14)*35
+		return occur, straight + flush*37
+	return occur, max(10*occur[0][1] + 4*occur[1][1], flush*37)		
+
+f = open('p054_poker.txt', 'r')
+print (sum(poker(line.split()) for line in f))
+f.close()
