f33a43d63078a8588b0af852dea8c7f03aa052a2
__panda <kevin.kuehler@gmail.com>
2016-10-28 22:32:12 -0700

    Add files via upload

---
+++ p065.py
@@ -0,0 +1,28 @@
+# Kevin Kuehler
+# https://github.com/kevinkuehler/Project-Euler
+#
+# Project Euler Problem 65
+# https://projecteuler.net/problem=65
+#
+
+
+# pass: num, denom of nth convergent, n-1
+# returns: num of nth convergent for e
+#
+# Uses the fact that every third sequence 
+# of e is an even number 2k, where k = s/3.
+# We add this number to our fraction and
+# swap num and denom since they are over one,
+# In B.C. the fraction is added to 2 and returned
+def e_convergent(num, denom, seq):
+	if seq == 1:
+		return int(2*denom + num)
+	next = 2*seq/3 if seq % 3 == 0 else 1
+	return e_convergent(denom, next*denom + num, seq - 1)
+
+# Sums digits of the 100th convergent
+def main():
+	print(sum(int(i) for i in str(e_convergent(1,1,99))))
+
+if __name__ == '__main__':
+	main()
