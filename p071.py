8d0d28f4abae036b5a98805caef38d966321a9ae
__panda <kevin.kuehler@gmail.com>
2016-11-13 01:31:25 -0800

    Add files via upload

---
+++ p071.py
@@ -0,0 +1,26 @@
+# Kevin Kuehler
+# https://github.com/kevinkuehler/Project-Euler
+#
+# Project Euler Problem 71
+# https://projecteuler.net/problem=71
+#
+
+from fractions import gcd
+
+# We know the neighbors a/b < c/d in a Farey sequence
+# have a difference of 1 / (bd). We are also given 
+# c/d = 3/7, therefore 3/7-a/b = 1 / (7b)
+# => a = (3b-1)/7, such that a is an integer, and
+# b <= L = 1,000,000 (largest denominator = set size)
+def main():
+    L, a = 1000000, -1
+
+    for b in range(L, 2, -1):
+        a = (3 * b - 1) / 7
+        if int(a) == a: break
+    print(int(a))
+
+if __name__ == '__main__':
+    main()
+
+
